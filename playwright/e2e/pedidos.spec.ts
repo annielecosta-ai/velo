286e6216dc0681956b64effab7750f299b3a095e
Anniele Bitu <annielecosta@yahoo.com.br>
2026-04-07 12:58:15 -0300

    iniciando a automação dos testes de consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,22 @@
+import { test, expect } from '@playwright/test';
+
+test('deve consultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+  //Checkpoint
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+  await page.getByTestId('header-nav').click();
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click();
+  await expect(page.getByRole('heading', { name: 'Consultar Pedido' })).toBeVisible();
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
+  await page.getByTestId('search-order-id').click();
+  await expect(page.getByTestId('search-order-id')).toBeVisible();
+  await page.getByTestId('search-order-id').click();
+  await page.getByTestId('search-order-id').fill('VLO-WPZSFR');
+  await page.getByTestId('search-order-id').click();
+  await page.getByTestId('search-order-button').click();
+  await expect(page.getByTestId('order-result-id')).toBeVisible();
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-WPZSFR');
+  await expect(page.getByTestId('order-result-status')).toBeVisible();
+  await page.getByTestId('order-result-status').click();
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+});
